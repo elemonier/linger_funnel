583d0899344dad8bdb6c1f09102f33a820d73a7c
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-04 14:29:58 -0500

    updates to pull username from url for dash

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+import requests
+
 
 app = Flask(__name__)
 
@@ -13,13 +15,21 @@ db = SQLAlchemy(app)
 def login():
 	return render_template("login.html")
 
+# @app.route("/search/<search_query>")
+# def search(search_query):
+# 	return search_query
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return render_template("dashboard.html", username=username)
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
 
-@app.route("/search/<search_query>")
-def search(search_query):
-	return search_query
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
