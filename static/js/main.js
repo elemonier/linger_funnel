1466564ff50f6adfac2c5de7a4ac1ee95f10523d
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-06 13:50:29 -0500

    lots

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,19 @@
 
+$( document ).ready(function() {
+
+	console.log($(location).attr('pathname'));
+	setInterval('shift_about_carousel()', 2000);
+
+
+
+
+});
+
+function shift_about_carousel() {
+	$('.about-carousel-on').addClass('about-carousel-off');
+	$('.about-carousel-on').removeClass('about-carousel-on');
+	$('.about-carousel-next').addClass('about-carousel-on');
+	$('.about-carousel-next').removeClass('about-carousel-next');
+	$('.about-carousel-off').addClass('about-carousel-next');
+	$('.about-carousel-off').removeClass('about-carousel-off');
+}
