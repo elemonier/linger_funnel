a70f8a13fcb867f5198cd62a7f14b7e395d2fb48
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-07 03:32:46 -0500

    SO MUCH GOOD

--- static/js/main.js
+++ static/js/main.js
@@ -2,18 +2,8 @@
 $( document ).ready(function() {
 
 	console.log($(location).attr('pathname'));
-	// setInterval('shift_about_carousel()', 2000);
-
-
-
+	$("#myCarousel").carousel({
+	         interval : 2000
+	     });
 
 });
-
-// function shift_about_carousel() {
-// 	$('.about-carousel-on').addClass('about-carousel-off');
-// 	$('.about-carousel-on').removeClass('about-carousel-on');
-// 	$('.about-carousel-next').addClass('about-carousel-on');
-// 	$('.about-carousel-next').removeClass('about-carousel-next');
-// 	$('.about-carousel-next-next').addClass('about-carousel-next');
-// 	$('.about-carousel-next-next').removeClass('about-carousel-next-next');
-// }
